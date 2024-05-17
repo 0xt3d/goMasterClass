9d95ad7b43180fbdf72b254e7c85863f8ec6ee70
0xt3d <Aditya.Patil0404@gmail.com>
2024-05-17 22:53:02 +0530

    Debuged Error
    Pervious version did not printed the the variable name from hello. that issue has been fixed in this commit

--- greetings/greeting.go
+++ greetings/greeting.go
@@ -2,11 +2,9 @@ package greetings
 
 import (
 	"fmt"
-
-	"golang.org/x/text/message"
 )
 
 func Hello(name string) string {
-	message := fmt.Sprint("Hi, %v. Welcome!, name")
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
 }
