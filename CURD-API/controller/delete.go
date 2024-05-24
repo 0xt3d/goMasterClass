fb0af3d8a0ecbca432c1513093e7af5351335634
0xt3d <Aditya.Patil0404@gmail.com>
2024-05-24 22:42:30 +0530

    Project Init

---
+++ CURD-API/controller/delete.go
@@ -0,0 +1,17 @@
+func DeleteBook(c *gin.Context) {
+	var book database.Book
+	id := c.Param("id")
+	res := database.DB.Find(&book, id)
+	if res.RowsAffected == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "book not found",
+		})
+		return
+	}
+	database.DB.Delete(&book)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "book deleted successfully",
+	})
+	return
+ }
+ 
---
+++ CURD-API/controller/gin.go
@@ -0,0 +1,9 @@
+package controllers
+ 
+import (
+   "errors"
+   "net/http"
+ 
+   "example.com/go-crud-api/database"
+   "github.com/gin-gonic/gin"
+)
