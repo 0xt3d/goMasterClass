7bc9598ae09d2c2a78245ec29cbfb14767c6069e
0xt3d <Aditya.Patil0404@gmail.com>
2024-07-07 20:36:04 +0530

    Implementing simple logic for selection sort

---
+++ basics/algorithms/selectionSort.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func selection(array [int)int  {
+	n := len(array)
+	for i:=0; i< n-1; i++{
+		minIndex :i
+		for j := i+1 ;j<n; j++{
+			
+		}
+			
+		} else {
+			
+		}
+	}
+	
+}
