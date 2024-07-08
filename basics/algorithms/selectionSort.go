b12588d1e72a7cd926b9505f2b5cb04ce1fe1e58
0xt3d <Aditya.Patil0404@gmail.com>
2024-07-08 21:45:34 +0530

    Implemented Selection Sort

--- basics/algorithms/selectionSort.go
+++ basics/algorithms/selectionSort.go
@@ -1,18 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func selection(array [int)int  {
-	n := len(array)
-	for i:=0; i< n-1; i++{
-		minIndex :i
-		for j := i+1 ;j<n; j++{
-			
+func selectionSort(arr []int) {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		minIndex := i
+		for j := i + 1; j < n; j++ {
+			if arr[j] < arr[minIndex] {
+				minIndex = j
+			}
 		}
-			
-		} else {
-			
-		}
+		arr[i], arr[minIndex] = arr[minIndex], arr[i]
 	}
-	
+}
+
+func main() {
+	arr := []int{64, 34, 25, 12, 22, 11, 90}
+	fmt.Println("Original array:", arr)
+
+	selectionSort(arr)
+	fmt.Println("Sorted array:", arr)
 }
