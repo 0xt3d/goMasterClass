4b49899864a9d5e02aa1b448b205d4cc01e5d44b
0xt3d <Aditya.Patil0404@gmail.com>
2024-07-11 20:24:33 +0530

    Merging all the Sorting Algorithms

---
+++ basics/algorithms/sort.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func selectionSort(arr []int) {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		minIndex := i
+		for j := i + 1; j < n; j++ {
+			if arr[j] < arr[minIndex] {
+				minIndex = j
+			}
+		}
+		arr[i], arr[minIndex] = arr[minIndex], arr[i]
+	}
+}
+
+func merge(left, right []int) []int {
+    result := make([]int, 0, len(left)+len(right))
+    i, j := 0, 0
+
+    for i < len(left) && j < len(right) {
+        if left[i] <= right[j] {
+            result = append(result, left[i])
+            i++
+        } else {
+            result = append(result, right[j])
+            j++
+        }
+    }
+
+    result = append(result, left[i:]...)
+    result = append(result, right[j:]...)
+
+    return result
+}
