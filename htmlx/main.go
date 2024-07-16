2bf62e873ba0608fd24d76624d98a126f062ff9d
0xt3d <Aditya.Patil0404@gmail.com>
2024-07-16 18:12:58 +0530

    Implemented Web Server

--- htmlx/main.go
+++ htmlx/main.go
@@ -1,6 +1,15 @@
 package main
 
 import(
-	"fmt"
-	
+	"github.com/gofiber/fiber/v2"
 )
+
+func main()  {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		c.Type("html")
+		return c.SendString("Hello, World")
+	})
+	app.Listen(":3030")
+}
