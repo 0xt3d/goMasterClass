f3710c2aaaa5c68636591940b86b9a8daccd4d0f
0xt3d <Aditya.Patil0404@gmail.com>
2024-07-24 22:00:27 +0530

    Understanding the basics

--- htmlx/main.go
+++ htmlx/main.go
@@ -8,6 +8,9 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
 	})
-
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "about.html")
+	})
+	
 	http.ListenAndServe(":8080", nil)
 }
