ce38610119e663948bdb3ac26f952adce0382839
0xt3d <Aditya.Patil0404@gmail.com>
2024-07-25 22:35:04 +0530

    Taking a different route

--- htmlx/main.go
+++ htmlx/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-	"net/http"
+    "net/http"
+    "github.com/mjibson/esc"
 )
 
+//go:generate esc -o templates.go -pkg main templates.htmlx
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
-	})
-	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "about.html")
-	})
-	
-	http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        data := struct {
+            Title   string
+            Message string
+        }{
+            Title:   "Simple Go HTMLX Example",
+            Message: "Hello, World!",
+        }
+        if err := templates.Index.Execute(w, data); err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
+    })
+
+    http.ListenAndServe(":8080", nil)
 }
