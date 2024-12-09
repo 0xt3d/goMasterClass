e61c7fbc6d997e0a11045f68ccb981c72c6da8e5
0xt3d <Aditya.Patil0404@gmail.com>
2024-12-09 22:39:24 +0530

    updated

--- basics/patterns/trangle.go
+++ basics/patterns/trangle.go
@@ -15,3 +15,20 @@ func main() {
         fmt.Println()
     }
 }
+package main
+
+import "fmt"
+
+func main() {
+    rows := 5
+    for i := 1; i <= rows; i++ {
+        for j := 1; j <= i; j++ {
+            if i == j || j == 1 || i == rows {
+                fmt.Print("*")
+            } else {
+                fmt.Print(" ")
+            }
+        }
+        fmt.Println()
+    }
+}
