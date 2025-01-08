463dd7efcc908b8c1e6f957d010049448b39fb90
0xt3d <Aditya.Patil0404@gmail.com>
2025-01-09 00:00:42 +0530

    Updated

--- Intro.go
+++ Intro.go
@@ -5,3 +5,12 @@ import "fmt"
 func main() {
     fmt.Println("Hello, World!")
 }
+
+func variables(){
+	var integerVar int = 42
+    var floatVar float64 = 3.14
+    var stringVar string = "Hello, Golang!"
+    var boolVar bool = true
+
+    fmt.Println(integerVar, floatVar, stringVar, boolVar)
+}
